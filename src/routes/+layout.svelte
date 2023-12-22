a8ceb39e2b235b208b4d4038246226a2cb30b5cb
pedroinciarte <pedro.inciarte13@gmail.com>
2023-12-22 18:04:12 -0300

    Add shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.pcss';
+	
 </script>
 
 <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$courses: 'src/courses'
+			$courses: 'src/courses',
+			$lib: "./src/lib"
 		}
 	}
 };
