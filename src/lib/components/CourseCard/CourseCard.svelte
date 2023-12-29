1397f8d7526d19a045d80e471d9418cdfa731b2a
pedroinciarte <pedro.inciarte13@gmail.com>
2023-12-29 13:13:00 -0300

    fix: change component name

--- src/lib/components/CourseCard/CourseCard.svelte
+++ src/lib/components/CourseCard/CourseCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { CourseOverviewWithSlug } from "$courses/types/course-overview.interface";
-    import BlackBackgroundParagraph from "./components/BlackBackgroundParagraph.svelte";
+    import BlackBackgroundCourseInfo from "./components/BlackBackgroundCourseInfo.svelte";
 
     export let course: CourseOverviewWithSlug;
 
@@ -10,12 +10,12 @@
 
 <div class="grid grid-cols-3 gap-2 border-r-4 border-l-2 border-t-2 border-b-4 border-black">
     <div class="col-span-1 bg-{course.color}-300 w-[15ch]">
-        <BlackBackgroundParagraph text={level}/>
+        <BlackBackgroundCourseInfo text={level}/>
        <img src="/Cat.png" alt="course cat"/>
     </div>
     <div class="col-span-2 flex-col gap-6 px-1 pt-4">
         <h1 class="font-vt323 text-lg">{course.name.toUpperCase()}</h1>
-        <BlackBackgroundParagraph text={course.subject}/>
+        <BlackBackgroundCourseInfo text={course.subject}/>
     </div>
 </div>
 
