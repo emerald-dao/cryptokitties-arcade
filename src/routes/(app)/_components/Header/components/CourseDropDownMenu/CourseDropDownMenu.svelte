89b9619eab865758fd17e6a352d5cc3e2424f338
pedroinciarte <pedro.inciarte13@gmail.com>
2023-12-29 13:13:50 -0300

    fix: change background color for dropdown menu trigger

--- src/routes/(app)/_components/Header/components/CourseDropDownMenu/CourseDropDownMenu.svelte
+++ src/routes/(app)/_components/Header/components/CourseDropDownMenu/CourseDropDownMenu.svelte
@@ -13,7 +13,7 @@
 <DropdownMenu.Root>
   <DropdownMenu.Trigger asChild let:builder>
     <Button builders={[builder]} variant="outline"
-      class="h-full rounded-none border-x border-black border-y font-vt323 text-base {builder["data-state"]=="open" ? 'bg-black text-white' : 'bg-white text-black'}">{level}
+      class="h-full rounded-none border-x border-black border-y font-vt323 text-base {builder["data-state"]=="open" ? 'bg-[#2A0202] text-white' : 'bg-white text-black'}">{level}
     </Button>
   </DropdownMenu.Trigger>
   <DropdownMenu.Content class="rounded-none border-r-4 border-b-4 border-t-2 border-l-2 border-black">
