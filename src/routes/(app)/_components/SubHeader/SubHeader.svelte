c775cbfc7bf30f29efec318ed9bb5a7601b4f68a
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-16 12:20:09 -0300

    Add subheader component and shadcn progress bar

--- src/routes/(app)/_components/SubHeader/SubHeader.svelte
+++ src/routes/(app)/_components/SubHeader/SubHeader.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import type { CourseOverviewWithChapters } from '$courses/types/course-overview.interface';
 	import Chapter from './components/Chapter.svelte';
+	import ProgressBar from './components/ProgressBar.svelte';
 
 	export let course: CourseOverviewWithChapters;
 </script>
 
-<div class="h-30 w-full items-center border-y border-black px-20">
-	{#each course.chapters as chapter, i}
-		<Chapter {chapter} number={i + 1} />
-	{/each}
+<div class=" flex items-center justify-between gap-x-3.5 border-y border-black px-20">
+	<div class="flex flex-wrap place-items-center">
+		{#each course.chapters as chapter, i}
+			<div class="w-fit border border-black">
+				<Chapter {chapter} number={i + 1} />
+			</div>
+		{/each}
+	</div>
+	<!--TODO: ask-->
+	<ProgressBar value={90} />
 </div>
--- src/routes/(app)/_components/SubHeader/components/Chapter.svelte
+++ src/routes/(app)/_components/SubHeader/components/Chapter.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	import type { ChapterOverviewWithSlug } from '$courses/types/chapter-overview.interface';
+	import { page } from '$app/stores';
 
 	export let chapter: ChapterOverviewWithSlug;
 	export let number: number;
+
+	$: path = $page.url.pathname.split('/');
+	$: activeCourse = path[2] + '/' + path[3];
+	$: isCurrentChapter = chapter.slug === activeCourse;
 </script>
 
-<div class="font-vt323 h-full w-fit border-x border-black px-3 text-base">
-	<p>{number}. {chapter.name.toUpperCase()}</p>
+<div class="{isCurrentChapter ? 'bg-black text-white' : ''} flex h-8 items-center">
+	<a
+		href="/courses/{chapter.slug}"
+		class="font-vt323 w-fit px-3 text-base
+     uppercase"
+	>
+		{number}. {chapter.name}
+	</a>
 </div>
