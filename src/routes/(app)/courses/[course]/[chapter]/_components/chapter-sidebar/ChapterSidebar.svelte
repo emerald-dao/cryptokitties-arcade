767a1b24d12fb5837c93ca6b4acd0ce0b7f3d6c9
mateoroldos <mateoroldos19@gmail.com>
2024-01-17 10:24:41 -0300

    fix: Clean unnecesary class in ChapterSidebar

--- src/routes/(app)/courses/[course]/[chapter]/_components/chapter-sidebar/ChapterSidebar.svelte
+++ src/routes/(app)/courses/[course]/[chapter]/_components/chapter-sidebar/ChapterSidebar.svelte
@@ -10,7 +10,7 @@
 
 <div
 	class:px-6={isSidebarOpen}
-	class="relative flex flex-col items-start gap-3 border-r border-black bg-slate-100 py-6"
+	class="relative flex flex-col gap-3 border-r border-black bg-slate-100 py-6"
 >
 	<LessonList bind:isSidebarOpen {chapter} />
 	<ToggleSidebarButton bind:isSidebarOpen />
