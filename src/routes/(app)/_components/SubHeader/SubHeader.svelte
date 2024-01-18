dfab000d27415237fbe3c807278a38257f79aea4
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-18 12:26:21 -0300

    change more details

--- src/routes/(app)/_components/SubHeader/SubHeader.svelte
+++ src/routes/(app)/_components/SubHeader/SubHeader.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import type { CourseOverviewWithChapters } from '$courses/types/course-overview.interface';
-	import Chapter from './atoms/CourseChapterLabel.svelte';
+	import CourseChapterLabel from './atoms/CourseChapterLabel.svelte';
 	import ProgressBar from './atoms/ProgressBar.svelte';
 
 	export let course: CourseOverviewWithChapters;
 </script>
 
 <div class="flex items-center justify-between gap-x-3.5 border-y border-black px-20">
-	<div class="flex flex-wrap place-items-center border-b border-black">
+	<div class="flex flex-wrap place-items-center">
 		{#each course.chapters as chapter, i}
-			<Chapter {chapter} chapterNumber={i + 1} />
+			<CourseChapterLabel {chapter} chapterNumber={i + 1} />
 		{/each}
 	</div>
 	<!--TODO: progressbar value-->
--- src/routes/(app)/_components/SubHeader/atoms/CourseChapterLabel.svelte
+++ src/routes/(app)/_components/SubHeader/atoms/CourseChapterLabel.svelte
@@ -8,15 +8,12 @@
 	$: isChapterActive = $page.params.chapter === chapter.slug.split('/').pop();
 </script>
 
-<div class:is-active={isChapterActive} class="flex items-center border-x border-black py-1">
+<div
+	class:bg-black={isChapterActive}
+	class:text-white={isChapterActive}
+	class="flex items-center border-x border-black py-1"
+>
 	<a href="/courses/{chapter.slug}" class="font-pixel px-3 uppercase">
 		{chapterNumber}. {chapter.name}
 	</a>
 </div>
-
-<style>
-	.is-active {
-		background-color: black;
-		color: white;
-	}
-</style>
