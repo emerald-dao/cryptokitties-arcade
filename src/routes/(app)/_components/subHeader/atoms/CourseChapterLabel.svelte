c3edad3aa91a401d547f760cef7920d4316ddbcf
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-18 13:07:21 -0300

    Add border-b to chapter component

--- src/routes/(app)/_components/subHeader/atoms/CourseChapterLabel.svelte
+++ src/routes/(app)/_components/subHeader/atoms/CourseChapterLabel.svelte
@@ -11,7 +11,7 @@
 <div
 	class:bg-black={isChapterActive}
 	class:text-white={isChapterActive}
-	class="flex items-center border-x border-black py-1"
+	class="flex items-center border-x border-b border-black py-1"
 >
 	<a href="/courses/{chapter.slug}" class="font-pixel px-3 uppercase">
 		{chapterNumber}. {chapter.name}
