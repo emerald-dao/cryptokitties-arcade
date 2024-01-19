76ecaacc4999dcc8ed0fe0a7725d10f9b31d9725
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-18 22:14:24 -0300

    fix: Change navBar for MarketingHeaderNavigation in marketing header

--- src/routes/(marketing)/_components/header/MarketingHeader.svelte
+++ src/routes/(marketing)/_components/header/MarketingHeader.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import NavBar from './components/NavBar.svelte';
+	import MarketingHeaderNavigation from './components/MarketingHeaderNavigation.svelte';
 	import FlowCatsLogo from '$lib/components/atoms/FlowCatsLogo.svelte';
 </script>
 
 <div class="flex justify-between border-b border-black px-36 py-8">
 	<FlowCatsLogo />
-	<NavBar />
+	<MarketingHeaderNavigation />
 </div>
