a62f183e52df34fa46c3237dbdd19a9d77536cb2
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-01-23 12:21:51 -0300

    feat: delete unnecesary course name, excerpt and chapters from course layout

--- src/routes/(app)/courses/[course]/+layout.svelte
+++ src/routes/(app)/courses/[course]/+layout.svelte
@@ -8,20 +8,5 @@
 <AppHeader activeCourse={data.course} allCourses={data.courses} />
 <AppSubHeader course={data.course} />
 <section class="flex h-screen flex-col bg-slate-50">
-	<div class="flex flex-col gap-5 border-b bg-slate-200 p-6">
-		<div>
-			<h1 class="text-2xl">{data.course.name}</h1>
-			<p>{data.course.excerpt}</p>
-		</div>
-		<div class="flex flex-row items-center">
-			{#each data.course.chapters as chapter}
-				<ul>
-					<li>
-						<a href={`/courses/${chapter.slug}`}>{chapter.name} chapter</a>
-					</li>
-				</ul>
-			{/each}
-		</div>
-	</div>
 	<slot />
 </section>
