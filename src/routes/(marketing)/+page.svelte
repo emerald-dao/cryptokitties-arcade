7ca156b5f4b03aa39b5dec26dea64cb190da60f4
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-24 11:40:36 -0300

    feat: Add main section cat images

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
+	import MainSection from './_components/home-page/sections/main-section/MainSection.svelte';
+
 	export let data;
 </script>
 
-<section class="flex flex-col gap-4 items-center justify-center">
-	<h1 class="text-2xl">Crypto Cats</h1>
-	<p>Marketing page</p>
-	<div class="flex flex-col items-center">
-		<h2 class="text-lg">Courses</h2>
-		{#each data.courses as course}
-			<ul>
-				<li>
-					<a href={`/courses/${course.slug}`}>
-						{course.name}
-					</a>
-				</li>
-			</ul>
-		{/each}
-	</div>
-</section>
+<MainSection />
---
+++ src/routes/(marketing)/_components/home-page/sections/main-section/MainSection.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import CatImageWithBackground from './atoms/CatImageWithBackground.svelte';
+	let images = ['complete-cat', 'complete-cat', 'complete-cat'];
+	let colors: ('pink' | 'purple' | 'yellow' | 'green')[] = ['pink', 'purple', 'yellow'];
+</script>
+
+<section class="grid grid-cols-4 grid-rows-2 gap-4">
+	<div class="row-span-2">
+		<CatImageWithBackground image={'rainbow-cat'} color={'green'} />
+	</div>
+	{#each images as image, i}
+		<CatImageWithBackground {image} color={colors[i]} />
+	{/each}
+	<div class="col-span-3 col-start-2">9</div>
+</section>
