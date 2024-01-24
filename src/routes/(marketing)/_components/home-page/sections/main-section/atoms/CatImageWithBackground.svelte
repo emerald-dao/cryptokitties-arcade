a127af592cfbf94162de8c653a480ea8db5b9a66
pedroinciarte <pedro.inciarte13@gmail.com>
2024-01-24 11:40:36 -0300

    feat: Add main section cat images

---
+++ src/routes/(marketing)/_components/home-page/sections/main-section/atoms/CatImageWithBackground.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let color: 'green' | 'pink' | 'purple' | 'yellow';
+	export let image: string;
+
+	const colorVariants = {
+		green: 'bg-green-300',
+		pink: 'bg-red-300',
+		purple: 'bg-purple-300',
+		yellow: 'bg-yellow-300'
+	};
+</script>
+
+<div class="{colorVariants[color]} border border-black object-center">
+	<img src="/{image}.png" alt="" />
+</div>
