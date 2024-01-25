7d7a4ce382f0632568b66b346e0d486e2e571c53
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-01-25 10:43:28 -0300

    feat: configure and set up the code editor

---
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/_components/code-editor/CodeEditor.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+	export let tabContent: { startingCode: string; solutionCode: string };
+
+	let editor: Monaco.editor.IStandaloneCodeEditor;
+	let monaco: typeof Monaco;
+	let editorContainer: HTMLElement;
+
+	async function initializeMonaco() {
+		monaco = (await import('./monaco')).default;
+		if (editorContainer && monaco) {
+			editor = monaco.editor.create(editorContainer, {
+				automaticLayout: true,
+				scrollBeyondLastLine: false,
+				theme: 'vs-dark'
+			});
+
+			const model = monaco.editor.createModel(tabContent.startingCode, 'javascript');
+			editor.setModel(model);
+		}
+	}
+
+	onMount(() => {
+		initializeMonaco();
+	});
+
+	$: {
+		if (editor) {
+			const model = editor.getModel();
+			if (model) {
+				model.setValue(tabContent.startingCode);
+			} else {
+				const newModel = monaco.editor.createModel(tabContent.startingCode, 'javascript');
+				editor.setModel(newModel);
+			}
+		}
+	}
+
+	onDestroy(() => {
+		monaco?.editor.getModels().forEach((model) => model.dispose());
+		editor?.dispose();
+	});
+</script>
+
+<div class="flex-1" bind:this={editorContainer} />
