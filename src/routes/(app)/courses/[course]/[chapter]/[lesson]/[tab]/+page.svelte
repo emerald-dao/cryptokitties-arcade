db9248cbf88ad7a2a2f36dd44ed78fdb9a915ae8
pedroinciarte <pedro.inciarte13@gmail.com>
2024-02-06 01:50:13 -0300

    feat: add correct answer and lesson redirects

--- src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/+page.svelte
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import TabContent from './_components/tab-content/TabContent.svelte';
+	import CheckAnswerButton from './_components/check-answer/CheckAnswerButton.svelte';
 
 	export let data;
 </script>
 
 <TabContent tabContent={data.tabContent} tabOverview={data.tabOverview} />
+{#if data.tabContent.type == 'code'}
+	<div class="grid grid-cols-2">
+		<CheckAnswerButton
+			color={data.course.color}
+			tabOverview={data.tabOverview}
+			tabContent={data.tabContent}
+			activeCourse={data.course}
+			activeChapter={data.chapter}
+		/>
+	</div>
+{/if}
---
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/_components/check-answer/CheckAnswerButton.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { COURSES_COLORS } from '$courses/constants/colors';
+	import type { LessonTabOverviewWithSlug } from '$courses/types/lesson-tab-overview.interface';
+	import * as Popover from '$lib/components/ui/popover';
+	import { Button } from '$lib/components/ui/button';
+	import persistentWritable from '$lib/stores/custom/persistentWritable';
+	import type { TabContentWithType } from '../../_types/tab-content.type';
+	import type { CourseOverviewWithChapters } from '$courses/types/course-overview.interface';
+	import type { ChapterOverviewWithLessons } from '$courses/types/chapter-overview.interface';
+	import CorrectAnswer from './atoms/CorrectAnswer.svelte';
+
+	export let color: keyof typeof COURSES_COLORS;
+	export let tabContent: TabContentWithType;
+	export let tabOverview: LessonTabOverviewWithSlug;
+	export let activeCourse: CourseOverviewWithChapters;
+	export let activeChapter: ChapterOverviewWithLessons;
+
+	let userCode: string;
+	let codeStore = persistentWritable<string>(tabOverview.slug, '');
+
+	let correctAnswer: boolean;
+
+	function handleCheckAnswer() {
+		if (tabContent.type == 'code') {
+			codeStore.subscribe((value) => {
+				userCode = value;
+			});
+			const normalizedSolutionCode = normalizeCode(tabContent.content.solutionCode);
+			const normalizedUserCode = normalizeCode(userCode);
+			console.log(normalizedSolutionCode);
+			console.log(normalizedUserCode);
+			correctAnswer = normalizedUserCode === normalizedSolutionCode;
+		}
+	}
+
+	function normalizeCode(code: string) {
+		const codeWithoutComments = code.replace(/\/\/.*$/gm, '');
+		return codeWithoutComments.replace(/\s/g, '');
+	}
+</script>
+
+<Popover.Root>
+	<Popover.Trigger asChild let:builder>
+		<Button
+			builders={[builder]}
+			variant="secondary"
+			class="{COURSES_COLORS[color].checkAnswer} rounded-none"
+			on:click={handleCheckAnswer}>CHECK ANSWER</Button
+		>
+	</Popover.Trigger>
+	<Popover.Content class="w-26">
+		{#if correctAnswer}
+			<CorrectAnswer {activeCourse} {activeChapter} />
+		{/if}
+	</Popover.Content>
+</Popover.Root>
