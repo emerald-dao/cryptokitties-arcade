d07f0cce5802f01a1dcc1bc0ed2df506808d4e50
pedroinciarte <pedro.inciarte13@gmail.com>
2024-02-05 22:42:32 -0300

    feat: Add monaco-editor-diff and compare solution code with users code

--- src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/+page.svelte
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+	import CheckAnswerButton from '../_components/atoms/CheckAnswerButton.svelte';
 	import TabContent from './_components/tab-content/TabContent.svelte';
+	import CodeDiffSheet from './_components/code-diff-sheet/CodeDiffSheet.svelte';
 
 	export let data;
 </script>
 
 <TabContent tabContent={data.tabContent} tabOverview={data.tabOverview} />
+{#if data.tabContent.type == 'code'}
+	<div class="grid grid-cols-2">
+		<CodeDiffSheet
+			color={data.course.color}
+			tabContent={data.tabContent}
+			tabOverview={data.tabOverview}
+		/>
+		<CheckAnswerButton color={data.course.color} />
+	</div>
+{/if}
---
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/[tab]/_components/code-diff-sheet/CodeDiffSheet.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import * as Sheet from '$lib/components/ui/sheet';
+	import { Button } from '$lib/components/ui/button';
+	import { COURSES_COLORS } from '$courses/constants/colors';
+	import type { TabContentWithType } from '../../_types/tab-content.type';
+	import type { LessonTabOverviewWithSlug } from '$courses/types/lesson-tab-overview.interface';
+	import persistentWritable from '$lib/stores/custom/persistentWritable';
+	import { onDestroy } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+
+	export let color: keyof typeof COURSES_COLORS;
+	export let tabContent: TabContentWithType;
+	export let tabOverview: LessonTabOverviewWithSlug;
+
+	let userCode: string;
+	let codeStore = persistentWritable<string>(tabOverview.slug, '');
+	let monaco: typeof Monaco;
+	let monacoDiffEditor: Monaco.editor.IStandaloneDiffEditor;
+	let editorContainer: HTMLElement;
+
+	codeStore.subscribe((value) => {
+		userCode = value;
+	});
+
+	async function handleHint() {
+		monaco = (await import('../code-editor/monaco')).default;
+		if (editorContainer && monaco) {
+			monacoDiffEditor = monaco.editor.createDiffEditor(editorContainer, {
+				readOnly: true,
+				renderSideBySide: false,
+				originalEditable: false,
+				ignoreTrimWhitespace: true
+			});
+
+			if (tabContent.type == 'code') {
+				monacoDiffEditor.setModel({
+					original: monaco.editor.createModel(userCode, 'cadence'),
+					modified: monaco.editor.createModel(tabContent.content.solutionCode, 'cadence')
+				});
+			}
+		}
+	}
+
+	onDestroy(() => {
+		monacoDiffEditor?.dispose();
+	});
+</script>
+
+<Sheet.Root>
+	<Sheet.Trigger asChild let:builder>
+		<Button
+			variant="secondary"
+			builders={[builder]}
+			class="{COURSES_COLORS[color].askForHelp} rounded-none"
+			on:click={handleHint}>ASK FOR HELP</Button
+		>
+	</Sheet.Trigger>
+	<Sheet.Content side="right">
+		<Sheet.Header class="flex flex-row items-center justify-between">
+			<Sheet.Title class="font-pixel">Help!</Sheet.Title>
+			<img src="/Cat.png" alt="cat" class="w-18 h-12 pr-4" />
+		</Sheet.Header>
+		<div bind:this={editorContainer} class="h-96"></div>
+		<Sheet.Footer class="pt-6">
+			<Sheet.Close asChild let:builder>
+				<Button builders={[builder]} type="submit" class="font-pixel rounded-none bg-black "
+					>Close</Button
+				>
+			</Sheet.Close>
+		</Sheet.Footer>
+	</Sheet.Content>
+</Sheet.Root>
