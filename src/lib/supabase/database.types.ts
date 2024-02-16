e58ef9b1b4b59f66074050049d6662140afcace9
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-02-16 13:34:04 -0300

    feat: database typescript types

--- src/lib/supabase/database.types.ts
+++ src/lib/supabase/database.types.ts
@@ -34,127 +34,46 @@ export interface Database {
   }
   public: {
     Tables: {
-      claims: {
+      users: {
         Row: {
-          block_id: string | null
-          created_at: string | null
-          event_id: string | null
-          float_id: string
-          network: string | null
-          transaction_id: string | null
-          user_address: string
+          address: string
+          created_at: string
         }
         Insert: {
-          block_id?: string | null
-          created_at?: string | null
-          event_id?: string | null
-          float_id: string
-          network?: string | null
-          transaction_id?: string | null
-          user_address: string
+          address: string
+          created_at?: string
         }
         Update: {
-          block_id?: string | null
-          created_at?: string | null
-          event_id?: string | null
-          float_id?: string
-          network?: string | null
-          transaction_id?: string | null
-          user_address?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "claims_event_id_fkey"
-            columns: ["event_id"]
-            referencedRelation: "events"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      events: {
-        Row: {
-          created_at: string | null
-          creator_address: string
-          id: string
-          network: string | null
-        }
-        Insert: {
-          created_at?: string | null
-          creator_address: string
-          id: string
-          network?: string | null
-        }
-        Update: {
-          created_at?: string | null
-          creator_address?: string
-          id?: string
-          network?: string | null
+          address?: string
+          created_at?: string
         }
         Relationships: []
       }
-      floats_groups: {
-        Row: {
-          float_id: string
-          group_id: number
-        }
-        Insert: {
-          float_id: string
-          group_id: number
-        }
-        Update: {
-          float_id?: string
-          group_id?: number
-        }
-        Relationships: [
-          {
-            foreignKeyName: "floats_groups_group_id_fkey"
-            columns: ["group_id"]
-            referencedRelation: "groups"
-            referencedColumns: ["id"]
-          }
-        ]
-      }
-      groups: {
+      users_lessons_finished: {
         Row: {
           created_at: string
-          description: string | null
-          id: number
-          name: string
+          lesson_slug: string
           user_address: string
         }
         Insert: {
           created_at?: string
-          description?: string | null
-          id?: number
-          name: string
+          lesson_slug: string
           user_address: string
         }
         Update: {
           created_at?: string
-          description?: string | null
-          id?: number
-          name?: string
+          lesson_slug?: string
           user_address?: string
         }
-        Relationships: []
-      }
-      pinned_floats: {
-        Row: {
-          float_id: string
-          network: string | null
-          user_address: string
-        }
-        Insert: {
-          float_id: string
-          network?: string | null
-          user_address: string
-        }
-        Update: {
-          float_id?: string
-          network?: string | null
-          user_address?: string
-        }
-        Relationships: []
+        Relationships: [
+          {
+            foreignKeyName: "users_lessons_finished_user_address_fkey"
+            columns: ["user_address"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["address"]
+          }
+        ]
       }
     }
     Views: {
@@ -181,6 +100,7 @@ export interface Database {
           id: string
           name: string
           owner: string | null
+          owner_id: string | null
           public: boolean | null
           updated_at: string | null
         }
@@ -192,6 +112,7 @@ export interface Database {
           id: string
           name: string
           owner?: string | null
+          owner_id?: string | null
           public?: boolean | null
           updated_at?: string | null
         }
@@ -203,17 +124,11 @@ export interface Database {
           id?: string
           name?: string
           owner?: string | null
+          owner_id?: string | null
           public?: boolean | null
           updated_at?: string | null
         }
-        Relationships: [
-          {
-            foreignKeyName: "buckets_owner_fkey"
-            columns: ["owner"]
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          }
-        ]
+        Relationships: []
       }
       migrations: {
         Row: {
@@ -245,6 +160,7 @@ export interface Database {
           metadata: Json | null
           name: string | null
           owner: string | null
+          owner_id: string | null
           path_tokens: string[] | null
           updated_at: string | null
           version: string | null
@@ -257,6 +173,7 @@ export interface Database {
           metadata?: Json | null
           name?: string | null
           owner?: string | null
+          owner_id?: string | null
           path_tokens?: string[] | null
           updated_at?: string | null
           version?: string | null
@@ -269,6 +186,7 @@ export interface Database {
           metadata?: Json | null
           name?: string | null
           owner?: string | null
+          owner_id?: string | null
           path_tokens?: string[] | null
           updated_at?: string | null
           version?: string | null
@@ -277,6 +195,7 @@ export interface Database {
           {
             foreignKeyName: "objects_bucketId_fkey"
             columns: ["bucket_id"]
+            isOneToOne: false
             referencedRelation: "buckets"
             referencedColumns: ["id"]
           }
@@ -350,4 +269,84 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
 
