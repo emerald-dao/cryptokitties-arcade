fa3118e754fb1474bbbddecea05b614c9daba718
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-02-19 12:47:24 -0300

    feat: supabase createClient with Database types

--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_API_URL } from '$env/static/public';
 import { PRIVATE_SUPABASE_SERVICE_ROLE } from '$env/static/private';
+import type { Database } from '$lib/supabase/database.types';
 
-export const serviceSupabase = createClient(
+export const serviceSupabase = createClient<Database>(
   PUBLIC_SUPABASE_API_URL,
   PRIVATE_SUPABASE_SERVICE_ROLE
 );
