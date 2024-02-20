fc1acea87a645d65072f5c58f8f483b74fc7c306
pedroinciarte <pedro.inciarte13@gmail.com>
2024-02-20 17:25:02 -0300

    feat: Fetch and set user's completed lessons on component mount

--- src/lib/stores/user-finished-lessons/userFinishedLessonsStore.ts
+++ src/lib/stores/user-finished-lessons/userFinishedLessonsStore.ts
@@ -3,5 +3,5 @@ import { writable } from 'svelte/store';
 export const userFinishedLessons = writable<string[]>([]);
 
 export const addLessonFinishedSlug = (slug: string) => {
-    userFinishedLessons.update(lessonSlugs => [...lessonSlugs, slug]);
+	userFinishedLessons.update((lessonSlugs) => [...lessonSlugs, slug]);
 };
--- src/routes/(app)/courses/[course]/+layout.svelte
+++ src/routes/(app)/courses/[course]/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import AppSubHeader from '../../_components/app-sub-header/AppSubHeader.svelte';
 	import AppHeader from '../../_components/app-header/AppHeader.svelte';
+	import { onMount } from 'svelte';
+	import { getUserCompletedLessons } from '$lib/features/users/functions/getUserCompletedLessons';
+	import { user } from '$lib/stores/flow/FlowStore';
+	import { userFinishedLessons } from '$lib/stores/user-finished-lessons/userFinishedLessonsStore';
 
 	export let data;
+
+	onMount(async () => {
+		let finishedLessons = await getUserCompletedLessons($user.addr);
+		userFinishedLessons.set(finishedLessons);
+	});
 </script>
 
 <AppHeader activeCourse={data.course} allCourses={data.courses} />
