31497c393d163fd4edd811e4a2c0301497b9f679
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-02-21 11:29:45 -0300

    feat: make "prose" dynamic to theme changes

--- src/routes/(app)/courses/[course]/[chapter]/[lesson]/_components/lesson-article/LessonArticle.svelte
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/_components/lesson-article/LessonArticle.svelte
@@ -2,6 +2,6 @@
 	export let lessonContent: ConstructorOfATypedSvelteComponent;
 </script>
 
-<article class="all-prose p-6">
+<article class="all-prose p-6 dark:prose-invert">
 	<svelte:component this={lessonContent} />
 </article>
