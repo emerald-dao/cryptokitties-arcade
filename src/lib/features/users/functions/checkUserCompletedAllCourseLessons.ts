a08d1e37d2af3a2d740b24fc3ce3b542fbeb2825
pedroinciarte <pedro.inciarte13@gmail.com>
2024-02-21 13:47:44 -0300

    fix: Better way to fetch user completed lessons

--- src/lib/features/users/functions/checkUserCompletedAllCourseLessons.ts
+++ src/lib/features/users/functions/checkUserCompletedAllCourseLessons.ts
@@ -3,21 +3,23 @@ export const userCompletedAllCourseLessons = async (
 	courseSlug: string,
 	amountOfLessons: number
 ) => {
-	const res = await fetch(`/api/user-completed-lessons/${userAddress}`, {
-		method: 'GET',
-		headers: {
-			'content-type': 'application/json'
+	if (userAddress) {
+		const res = await fetch(`/api/user-completed-lessons/${userAddress}`, {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const response = await res.json();
+
+		const finishedCourseLessons = response.filter((res: { lesson_slug: string }) =>
+			res.lesson_slug.includes(courseSlug)
+		);
+
+		if (amountOfLessons === finishedCourseLessons.length) {
+			return true;
 		}
-	});
-
-	const response = await res.json();
-
-	const finishedCourseLessons = response.filter((res: { lesson_slug: string }) =>
-		res.lesson_slug.includes(courseSlug)
-	);
-
-	if (amountOfLessons === finishedCourseLessons.length) {
-		return true;
 	}
 	return false;
 };
--- src/routes/(app)/courses/[course]/+layout.svelte
+++ src/routes/(app)/courses/[course]/+layout.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
 	import AppSubHeader from '../../_components/app-sub-header/AppSubHeader.svelte';
 	import AppHeader from '../../_components/app-header/AppHeader.svelte';
-	import { onMount } from 'svelte';
 	import { getUserCompletedLessons } from '$lib/features/users/functions/getUserCompletedLessons';
 	import { user } from '$lib/stores/flow/FlowStore';
 	import { userFinishedLessons } from '$lib/stores/user-finished-lessons/userFinishedLessonsStore';
 
 	export let data;
 
-	onMount(async () => {
-		let finishedLessons = await getUserCompletedLessons($user.addr);
-		userFinishedLessons.set(finishedLessons);
-	});
+	async function userCompletedLessons() {
+		return await getUserCompletedLessons($user.addr);
+	}
+
+	$: if ($user) {
+		userCompletedLessons().then((finishedLessons) => {
+			userFinishedLessons.set(finishedLessons);
+		});
+	}
 </script>
 
 <AppHeader activeCourse={data.course} allCourses={data.courses} />
