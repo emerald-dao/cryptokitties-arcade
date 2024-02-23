7f4e5ef688b12d49312f923a794e6d3e6b84ae7a
pedroinciarte <pedro.inciarte13@gmail.com>
2024-02-23 14:31:45 -0300

    feat: Change progress bar logic

--- src/routes/(app)/_components/app-sub-header/AppSubHeader.svelte
+++ src/routes/(app)/_components/app-sub-header/AppSubHeader.svelte
@@ -2,14 +2,25 @@
 	import type { CourseOverviewWithChapters } from '$courses/types/course-overview.interface';
 	import CourseChapterLabel from './atoms/CourseChapterLabel.svelte';
 	import ChapterProgressBar from './atoms/ChapterProgressBar.svelte';
-	import { page } from '$app/stores';
+	import { userFinishedLessons } from '$lib/stores/user-finished-lessons/userFinishedLessonsStore';
+	import { onDestroy } from 'svelte';
 
 	export let course: CourseOverviewWithChapters;
+	export let userCourseFinishedLessons: string[] = [];
+	let chapterProgress: number;
+	let courseAmountOfLessons = 0;
 
-	$: selectedLessonNumber = parseInt($page.params.lesson?.split('-')[0]) || 0;
-	$: chapter = course.chapters.find((c) => c.slug == course.slug + '/' + $page.params.chapter);
-	$: amountOfLessons = chapter?.lessons?.length || 0;
-	$: chapterProgress = Math.min((selectedLessonNumber * 100) / amountOfLessons, 100) || 0;
+	for (let i = 0; i < course.chapters.length; i++) {
+		courseAmountOfLessons += course.chapters[i].lessons.length;
+	}
+
+	const unsubscribe = userFinishedLessons.subscribe((value) => {
+		userCourseFinishedLessons = value.filter((lesson) => lesson.includes(course.slug));
+	});
+
+	$: chapterProgress = (userCourseFinishedLessons.length / courseAmountOfLessons) * 100;
+
+	onDestroy(() => unsubscribe);
 </script>
 
 <div class="flex items-center justify-between gap-x-3.5 border-y px-10">
