b16d2a6543a066643f32256dc958fc3feead1ac9
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-03-07 13:25:28 -0300

    feat: make FlowCatsLogo text bigger

--- src/lib/components/atoms/FlowCatsLogo.svelte
+++ src/lib/components/atoms/FlowCatsLogo.svelte
@@ -1,4 +1,4 @@
-<a href="/" class="flex items-center gap-0">
+<a href="/" class="flex items-center gap-0 text-xl">
 	<img src="/logo-cat.png" alt="logo" class="h-14 w-14" />
 	<p>FLOW CATS</p>
 </a>
