97ce4e5a2d4eb9b2e40e59bd569bdb0b80bbe467
mateoroldos <mateoroldos19@gmail.com>
2024-03-07 17:53:54 -0300

    feat: FlowConnect styling improvements

--- src/lib/components/atoms/FlowConnect.svelte
+++ src/lib/components/atoms/FlowConnect.svelte
@@ -3,6 +3,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import { Wallet } from 'lucide-svelte';
 	import { userFinishedLessons } from '$lib/stores/user-finished-lessons/userFinishedLessonsStore';
+	import Icon from '@iconify/svelte';
 
 	export let logIn: () => void;
 	export let unauthenticate: () => void;
@@ -17,17 +18,24 @@
 
 {#if user?.addr}
 	<div class="flex flex-row items-center justify-center gap-3">
-		{user.addr}
-		<Button on:click={logOut}>Disconnect</Button>
+		<div
+			class="relative inline-flex items-center justify-center bg-primary px-4 py-2 text-primary-foreground"
+		>
+			<Icon icon="pixelarticons:wallet" class="mr-1 size-4" />
+			{user.addr}
+		</div>
+		<button on:click={logOut}>
+			<Icon icon="pixelarticons:logout" class="size-4" />
+		</button>
 	</div>
 {:else}
 	<div class="flex items-center gap-3">
 		{#if showProgressTrackingPrompt}
 			<h4 class="text-sm">Connect to track progress</h4>
 		{/if}
-		<Button class="gap-1 px-4 py-2 uppercase" on:click={logIn}>
+		<Button class="gap-1 px-4 py-2" on:click={logIn}>
 			Connect
-			<Wallet strokeWidth={'1.5px'} class="h-4 w-4" />
+			<Icon icon="pixelarticons:wallet" class="size-4" />
 		</Button>
 	</div>
 {/if}
