17b3d25a323e998aaefcafd2b8e918812225fcdc
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-03-13 16:59:18 -0300

    feat: small adjustments on footer responsiveness

--- src/routes/(marketing)/_components/footer/Footer.svelte
+++ src/routes/(marketing)/_components/footer/Footer.svelte
@@ -3,62 +3,66 @@
 </script>
 
 <footer class="group border-t-2 border-solid border-border py-6 text-lg">
-	<div class="section flex flex-row justify-between gap-8 lg:gap-20">
-		<p class="group">
-			Built with
-			<Icon
-				icon="pixelarticons:heart"
-				class="inline text-red-400 group-hover:animate-bounce group-hover:text-green-400"
-			/>
-			by
-			<a
-				href="https://ecdao.org"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-			>
-				Emerald City DAO
-			</a>
-		</p>
-
-		<div class="flex flex-col lg:flex-row lg:space-x-4">
-			<a
-				href="https://academy.ecdao.org/en"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>Emerald Academy</a
-			>
-			<a
-				href="https://academy.ecdao.org/en"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>Flownaut</a
-			>
-			<a
-				href="https://toucans.ecdao.org/"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>Toucans</a
-			>
-			<a
-				href="https://toucans.ecdao.org/"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>FLOATs</a
-			>
+	<div class="section flex flex-row justify-between gap-6 md:flex-col lg:flex-row lg:gap-20">
+		<div>
+			<p class="group">
+				Built with
+				<Icon
+					icon="pixelarticons:heart"
+					class="inline text-red-400 group-hover:animate-bounce group-hover:text-green-400"
+				/>
+				by
+				<a
+					href="https://ecdao.org"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+				>
+					Emerald City DAO
+				</a>
+			</p>
 		</div>
-		<div class="flex flex-col lg:flex-row lg:space-x-4">
-			<a
-				href="https://discord.com/invite/emerald-city-906264258189332541"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>Discord</a
-			>
-			<a
-				href="https://ecdao.org"
-				target="_blank"
-				class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
-				>Twitter</a
-			>
+
+		<div class="flex gap-12 md:flex-row md:gap-20">
+			<div class="flex flex-col md:flex-row md:space-x-4">
+				<a
+					href="https://academy.ecdao.org/en"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>Emerald Academy</a
+				>
+				<a
+					href="https://academy.ecdao.org/en"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>Flownaut</a
+				>
+				<a
+					href="https://toucans.ecdao.org/"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>Toucans</a
+				>
+				<a
+					href="https://toucans.ecdao.org/"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>FLOATs</a
+				>
+			</div>
+			<div class="flex flex-col md:flex-row md:space-x-4">
+				<a
+					href="https://discord.com/invite/emerald-city-906264258189332541"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>Discord</a
+				>
+				<a
+					href="https://ecdao.org"
+					target="_blank"
+					class="underline decoration-gray-300 transition-all duration-300 ease-in-out hover:decoration-green-400"
+					>Twitter</a
+				>
+			</div>
 		</div>
 	</div>
 </footer>
