9004f4bb355cb29d40be5a0736d7fafeaacb86ac
ignaciodebat <112862961+IgnacioDebat@users.noreply.github.com>
2024-03-21 18:26:44 -0300

    feat: give lesson article and CodeEditor same width

--- src/routes/(app)/courses/[course]/[chapter]/[lesson]/+layout.svelte
+++ src/routes/(app)/courses/[course]/[chapter]/[lesson]/+layout.svelte
@@ -4,7 +4,7 @@
 	export let data;
 </script>
 
-<div class="grid h-full grid-cols-[420px_1fr] xl:grid-cols-[550px_1fr]">
+<div class="grid h-full grid-cols-[1fr_1fr]">
 	<div class="overflow-auto">
 		<LessonArticle lessonContent={data.lessonContent} />
 	</div>
