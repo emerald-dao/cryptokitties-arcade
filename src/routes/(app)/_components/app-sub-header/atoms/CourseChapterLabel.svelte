b834cd1481db5a23b992f33e0c2496f178b0046c
mateoroldos <mateoroldos19@gmail.com>
2024-03-25 20:08:32 -0300

    fix: Improve course chapters borders

--- src/routes/(app)/_components/app-sub-header/atoms/CourseChapterLabel.svelte
+++ src/routes/(app)/_components/app-sub-header/atoms/CourseChapterLabel.svelte
@@ -12,7 +12,7 @@
 <div
 	class:bg-primary={isChapterActive}
 	class:text-primary-foreground={isChapterActive}
-	class="flex items-center py-1 text-base"
+	class="flex items-center border-r-2 py-1 text-base"
 >
 	<a href="/courses/{chapter.slug}" class="text-nowrap px-3 uppercase" use:sound>
 		<span class="opacity-60">
