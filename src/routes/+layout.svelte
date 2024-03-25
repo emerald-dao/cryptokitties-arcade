db6cfa3b0af8ecfe9006ac491edf4edade3f7a75
mateoroldos <mateoroldos19@gmail.com>
2024-03-25 11:48:45 -0300

    feat: Add retro border

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,17 @@
 	import { setContext } from 'svelte';
 	import '../app.pcss';
 	import '../prism.css';
+	import { onNavigate } from '$app/navigation';
+
 	export let data;
 
 	setContext('coursesAmountOfLessons', data.coursesAmountOfLessons);
 </script>
 
-<div class="font-pixel">
-	<slot />
+<div class="flex h-svh overflow-hidden border bg-black font-pixel">
+	<div class="m-[1%] flex flex-1 overflow-hidden rounded-xl bg-white">
+		<div class="flex-1 overflow-hidden overflow-y-auto">
+			<slot />
+		</div>
+	</div>
 </div>
