a7c41fd40cb0c1b50b68fa67ffc526a689f10cf2
mateoroldos <mateoroldos19@gmail.com>
2024-04-10 15:11:30 -0300

    feat: Make ConnectPage cooler

---
+++ src/lib/utils/draggMeAction.ts
@@ -0,0 +1,29 @@
+function dragMe(node: HTMLElement) {
+	let moving = false;
+	let left = node.offsetLeft;
+	let top = node.offsetTop;
+
+	node.style.top = `${top}px`;
+	node.style.left = `${left}px`;
+	node.style.cursor = 'move';
+	node.style.userSelect = 'none';
+
+	node.addEventListener('mousedown', () => {
+		moving = true;
+	});
+
+	window.addEventListener('mousemove', (e) => {
+		if (moving) {
+			left += e.movementX;
+			top += e.movementY;
+			node.style.top = `${top}px`;
+			node.style.left = `${left}px`;
+		}
+	});
+
+	window.addEventListener('mouseup', () => {
+		moving = false;
+	});
+}
+
+export default dragMe;
