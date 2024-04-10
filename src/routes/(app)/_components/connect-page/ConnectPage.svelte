dd58a31bb43ea8924b7691cc5941047fd03901b2
mateoroldos <mateoroldos19@gmail.com>
2024-04-10 12:20:02 -0300

    feat: Base styling for loggin page

--- src/routes/(app)/_components/connect-page/ConnectPage.svelte
+++ src/routes/(app)/_components/connect-page/ConnectPage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import FlowConnect from '$lib/components/atoms/FlowConnect.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	import Icon from '@iconify/svelte';
+	import * as Card from '$lib/components/ui/card';
 	import { logIn, unauthenticate } from '../../../../flow/actions';
 	import { checkUser } from '$lib/features/users/functions/checkUser';
 	import { addUser } from '$lib/features/users/functions/postUser';
@@ -22,19 +21,41 @@
 	};
 </script>
 
-<section class="flex h-full flex-1 flex-col items-center justify-center gap-7 uppercase">
-	<Button variant="ghost" href="/" class="none group flex w-fit items-center gap-x-1 p-1">
-		<Icon icon="pixelarticons:arrow-left" />
-		GO HOME
-	</Button>
-	<FlowCatsLogo />
-	<p class="max-w-[40ch] text-center text-lg leading-tight">
-		Please connect your Flow wallet to continue
-	</p>
-	<FlowConnect
-		showProgressTrackingPrompt={false}
-		logIn={() => connect()}
-		{unauthenticate}
-		user={$user}
-	/>
+<section class="grid h-full grid-cols-[2fr_3fr] gap-7 uppercase">
+	<div class="flex flex-col justify-center gap-6 p-[20%]">
+		<div class="absolute left-10 top-6">
+			<FlowCatsLogo />
+		</div>
+		<p class="max-w-[40ch] text-2xl leading-tight">Please connect your Flow wallet to continue</p>
+		<FlowConnect
+			showProgressTrackingPrompt={false}
+			logIn={() => connect()}
+			{unauthenticate}
+			user={$user}
+		/>
+	</div>
+	<div
+		class="flex flex-col items-center justify-center gap-2 border-l-2 bg-green-200 p-[20%] text-4xl"
+	>
+		<h3 class="block">
+			#why<span class="rainbow-text-animated text-transparent">build</span>onflow
+		</h3>
+		<Card.Root class="w-full rounded border-2 shadow-md">
+			<Card.Header>
+				<Card.Description class="text-xl leading-tight"
+					>Because Flow is 100% EVM compatible which means that Ethereum contracts can be deployed
+					easily.</Card.Description
+				>
+			</Card.Header>
+			<Card.Footer class="flex flex-col items-start">
+				<div class="flex flex-row items-center gap-2">
+					<img src="/jacob-avatar.jpg" alt="Avatar" class="size-9 rounded-xl" />
+					<a href="https://twitter.com/jacobmtucker" target="_blank" class="text-xl underline"
+						>@jacobmtucker</a
+					>
+				</div>
+				<p class="text-lg">Emerald City founder.</p>
+			</Card.Footer>
+		</Card.Root>
+	</div>
 </section>
