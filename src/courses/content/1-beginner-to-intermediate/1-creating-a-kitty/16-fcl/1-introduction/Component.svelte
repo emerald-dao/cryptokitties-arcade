f08d56e5b085d161554d839ecc346fe05748be8b
jacob-tucker <jacobtucker818@gmail.com>
2024-04-15 14:37:22 -0400

    fix: last component

--- src/courses/content/1-beginner-to-intermediate/1-creating-a-kitty/16-fcl/1-introduction/Component.svelte
+++ src/courses/content/1-beginner-to-intermediate/1-creating-a-kitty/16-fcl/1-introduction/Component.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
+	import { FEATURED_KITTIES } from '../../../../../../routes/(marketing)/_components/home-page/sections/main-section/FEATURED_KITTIES';
+	import CatCard from '../../../../../../routes/(marketing)/_components/home-page/sections/main-section/atoms/CatCard.svelte';
+
 	let name: string;
 
-	let imageOptions = [
-		'/images/cats/vegetation-cat.png',
-		'/images/cats/sailor-cat.png',
-		'/images/cats/medieval-cat.png',
-		'/images/cats/magician-cat.png',
-		'/images/cats/fire-cat.png'
-	];
 	let chosenImageIndex = 0;
 
 	$: if (name) {
-		chosenImageIndex = Math.floor(Math.random() * 5) % 5;
+		chosenImageIndex =
+			Math.floor(Math.random() * FEATURED_KITTIES.length) % FEATURED_KITTIES.length;
 	}
 </script>
 
-<label for="name">Kitty Name</label>
-<input type="text" name="name" id="name" bind:value={name} />
+<input class="rounded border-2 text-lg" type="text" name="name" id="name" bind:value={name} />
 
-{#if name}
-	<img src={imageOptions[chosenImageIndex]} alt="kitty" />
-{/if}
+<div class="h-[500px] pt-2">
+	{#if name}
+		<CatCard {name} cat={FEATURED_KITTIES[chosenImageIndex]} />
+	{/if}
+</div>
